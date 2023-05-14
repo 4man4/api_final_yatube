80e7056e43d8be36ffff9a4fc6ef1dfc71a327f0
Петр Горюнов <petr.goryunov.89@ya.ru>
2023-05-14 19:19:46 +0500

    Готовность после второго ревью

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAuthorOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return True
+        return request.method in SAFE_METHODS or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or obj.author == request.user
